a111fe0c59530bb79164464fe00d45e27a679a67
Holden Altaffer <haltaffer@gmail.com>
2018-07-27 21:26:29 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,8 +13,8 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.new
-    @post.first_name = params[:first_name]
-    @post.last_name = params[:last_name]
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
     @student.save
     redirect_to student_pasth(@student)
   end
